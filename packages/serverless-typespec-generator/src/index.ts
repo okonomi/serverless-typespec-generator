b95e6316b8b8ab0df611d2d4b7d04a80d1cd3108
okonomi <okonomi@oknm.jp>
2025-04-29 16:01:40 +0900

    feat: implement typespec command

--- packages/serverless-typespec-generator/src/index.ts
+++ packages/serverless-typespec-generator/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Serverless TypeSpec Generator")
+import serverless from "serverless"
+import type Plugin from "serverless/classes/Plugin"
+
+export default class ServerlessTypeSpecGenerator implements Plugin {
+  hooks: Plugin.Hooks
+  commands: Plugin.Commands
+  serverless: serverless
+
+  constructor(serverless: serverless) {
+    console.log("Serverless TypeSpec Generator")
+
+    this.serverless = serverless
+    this.commands = {
+      'typespec': {
+        commands: {
+          'generate': {
+            options: {
+              'output-dir': {
+                usage: 'Output directory for generated TypeSpec files',
+                required: true,
+                type: 'string',
+              }
+            }
+          }
+        }
+      }
+    }
+
+    this.hooks = {}
+  }
+}
