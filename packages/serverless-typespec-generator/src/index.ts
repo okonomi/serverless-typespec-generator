9f0069cc00a64cca7831964f6e9ff491be832961
okonomi <okonomi@oknm.jp>
2025-04-29 16:41:51 +0900

    feat: stub generated typespec files

--- packages/serverless-typespec-generator/src/index.ts
+++ packages/serverless-typespec-generator/src/index.ts
@@ -1,13 +1,18 @@
 import type serverless from "serverless"
 import type Plugin from "serverless/classes/Plugin"
 
+import path from "node:path"
+
 export default class ServerlessTypeSpecGenerator implements Plugin {
   hooks: Plugin.Hooks
   commands: Plugin.Commands
   serverless: serverless
+  options: serverless.Options
 
-  constructor(serverless: serverless) {
+  constructor(serverless: serverless, options: serverless.Options) {
     this.serverless = serverless
+    this.options = options
+
     this.commands = {
       typespec: {
         commands: {
@@ -26,8 +31,74 @@ export default class ServerlessTypeSpecGenerator implements Plugin {
     }
 
     this.hooks = {
-      "typespec:generate:run": () => {
+      "typespec:generate:run": async () => {
         console.log("Generating TypeSpec files...")
+
+        const outputDir = path.resolve(
+          this.serverless.serviceDir,
+          String(options["output-dir"]),
+        )
+
+        await this.serverless.utils.writeFile(
+          path.join(outputDir, "tspconfig.yaml"),
+          `emit:
+  - "@typespec/openapi3"
+options:
+  "@typespec/openapi3":
+    emitter-output-dir: "{output-dir}/schema"
+    openapi-versions:
+      - 3.1.0
+`,
+        )
+
+        await this.serverless.utils.writeFile(
+          path.join(outputDir, "main.tsp"),
+          `import "@typespec/http";
+
+using Http;
+@service(#{ title: "Widget Service" })
+namespace DemoService;
+
+model Widget {
+  id: string;
+  weight: int32;
+  color: "red" | "blue";
+}
+
+model WidgetList {
+  items: Widget[];
+}
+
+@error
+model Error {
+  code: int32;
+  message: string;
+}
+
+model AnalyzeResult {
+  id: string;
+  analysis: string;
+}
+
+@route("/widgets")
+@tag("Widgets")
+interface Widgets {
+  /** List widgets */
+  @get list(): WidgetList | Error;
+  /** Read widgets */
+  @get read(@path id: string): Widget | Error;
+  /** Create a widget */
+  @post create(@body body: Widget): Widget | Error;
+  /** Update a widget */
+  @patch update(@path id: string, @body body: Widget): Widget | Error;
+  /** Delete a widget */
+  @delete delete(@path id: string): void | Error;
+
+  /** Analyze a widget */
+  @route("{id}/analyze") @post analyze(@path id: string): AnalyzeResult | Error;
+}
+`,
+        )
       },
     }
   }
