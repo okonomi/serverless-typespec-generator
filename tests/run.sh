c1637ff5fce2f93a92aff2de1ad964b83e51adc3
okonomi <okonomi@oknm.jp>
2025-05-05 22:44:40 +0900

    test: improve test runner

--- tests/run.sh
+++ tests/run.sh
@@ -3,14 +3,15 @@
 set -euo pipefail
 
 cd $(dirname "$0")
-
 cd sls-v3
 
-pnpm sls typespec generate --config basic.yml --output-dir basic/actual
-
-diff -u basic/actual/main.tsp basic/expected/main.tsp
-if [ $? -eq 0 ]; then
-  echo "success"
-else
-  echo "fail"
-fi
+for yml in *.yml; do
+  name="${yml%.yml}"
+  echo "Testing $yml ..."
+  pnpm sls typespec generate --config "$yml" --output-dir "$name/actual"
+  if diff -u "$name/actual/main.tsp" "$name/expected/main.tsp"; then
+    echo "success"
+  else
+    echo "fail"
+  fi
+done
