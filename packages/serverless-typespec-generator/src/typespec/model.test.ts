84248dfbd9fb48e9f18485553b8e11d0eed90f00
okonomi <okonomi@oknm.jp>
2025-05-09 22:28:58 +0900

    style: fix indent

--- packages/serverless-typespec-generator/src/typespec/model.test.ts
+++ packages/serverless-typespec-generator/src/typespec/model.test.ts
@@ -17,9 +17,9 @@ describe("render", () => {
         }
         const result = render(model)
         expect(result).toBe(dedent`
-        model TestModel {
-        }
-      `)
+          model TestModel {
+          }
+        `)
       })
     })
     context("with properties", () => {
@@ -58,12 +58,12 @@ describe("render", () => {
         }
         const result = render(model)
         expect(result).toBe(dedent`
-        model Mixed {
-          active: boolean;
-          tags: array;
-          meta: object;
-        }
-      `)
+          model Mixed {
+            active: boolean;
+            tags: array;
+            meta: object;
+          }
+        `)
       })
     })
   })
