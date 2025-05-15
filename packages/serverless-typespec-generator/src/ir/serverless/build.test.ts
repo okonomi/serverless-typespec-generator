2b4194a7ea7a18761ef9058c6183fcfc54e9782f
okonomi <okonomi@oknm.jp>
2025-05-15 10:48:24 +0900

    feat: listup functions with http event

--- packages/serverless-typespec-generator/src/ir/serverless/build.test.ts
+++ packages/serverless-typespec-generator/src/ir/serverless/build.test.ts
@@ -1,11 +1,121 @@
 import { describe, expect, it } from "vitest"
-import type { SLS } from "../../types/serverless"
+import { createServerlessMock } from "../../test/helper"
 import { buildServerlessIR } from "./build"
+import type { ServerlessIR } from "./type"
 
 describe("buildServerlessIR", () => {
   it("should build the serverless IR", () => {
-    const serverless = {} as SLS
+    const serverless = createServerlessMock({
+      hello: {
+        name: "hello",
+        handler: "handler.hello",
+        events: [
+          {
+            http: {
+              method: "get",
+              path: "/hello",
+            },
+          },
+        ],
+      },
+      world: {
+        name: "world",
+        handler: "handler.world",
+        events: [],
+      },
+    })
     const result = buildServerlessIR(serverless)
-    expect(result).toBeInstanceOf(Array)
+    expect(result).toEqual<ServerlessIR[]>([
+      {
+        kind: "function",
+        name: "hello",
+      },
+    ])
+  })
+  it("should handle functions without events", () => {
+    const serverless = createServerlessMock({
+      hello: {
+        name: "hello",
+        handler: "handler.hello",
+        events: [],
+      },
+    })
+    const result = buildServerlessIR(serverless)
+    expect(result).toEqual<ServerlessIR[]>([])
+  })
+  it("should handle functions with s3 events", () => {
+    const serverless = createServerlessMock({
+      hello: {
+        name: "hello",
+        handler: "handler.hello",
+        events: [
+          {
+            s3: {
+              bucket: "my-bucket",
+              event: "s3:ObjectCreated:*",
+            },
+          },
+        ],
+      },
+    })
+    const result = buildServerlessIR(serverless)
+    expect(result).toEqual<ServerlessIR[]>([])
+  })
+  it("should handle functions with multiple events", () => {
+    const serverless = createServerlessMock({
+      hello: {
+        name: "hello",
+        handler: "handler.hello",
+        events: [
+          {
+            sns: {
+              arn: "arn:aws:sns:us-east-1:123456789012:my-topic",
+            },
+          },
+          {
+            http: {
+              method: "get",
+              path: "/hello",
+            },
+          },
+        ],
+      },
+    })
+    const result = buildServerlessIR(serverless)
+    expect(result).toEqual<ServerlessIR[]>([
+      {
+        kind: "function",
+        name: "hello",
+      },
+    ])
+  })
+  it("should handle functions with multiple http events", () => {
+    const serverless = createServerlessMock({
+      hello: {
+        name: "hello",
+        handler: "handler.hello",
+        events: [
+          {
+            http: {
+              method: "get",
+              path: "/hello",
+            },
+          },
+          {
+            http: {
+              method: "post",
+              path: "/hello",
+            },
+          },
+        ],
+      },
+    })
+    const result = buildServerlessIR(serverless)
+    expect(result).toEqual<ServerlessIR[]>([
+      {
+        kind: "function",
+        name: "hello",
+      },
+    ])
   })
 })
--- packages/serverless-typespec-generator/src/ir/serverless/build.ts
+++ packages/serverless-typespec-generator/src/ir/serverless/build.ts
@@ -2,5 +2,21 @@ import type { Serverless } from "../../types/serverless"
 import type { ServerlessIR } from "./type"
 
 export function buildServerlessIR(serverless: Serverless): ServerlessIR[] {
-  return []
+  const irList: ServerlessIR[] = []
+
+  for (const functionName of serverless.service.getAllFunctions()) {
+    const events = serverless.service.getAllEventsInFunction(functionName)
+    const event = events.find((event) => "http" in event)
+    if (!event) {
+      // TODO: logging
+      continue
+    }
+
+    irList.push({
+      kind: "function",
+      name: functionName,
+    })
+  }
+
+  return irList
 }
